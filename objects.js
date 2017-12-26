e726a8a393e2f804db129b71fabf84c9233069fb
Ina Chemerko <innachemerko@gmail.com>
2017-12-26 02:56:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
 var playlist = {
+<<<<<<< HEAD
   artistName: "song1",
+=======
+  artist1: "song1",
+>>>>>>> ad16de4562245599b58210621b4279a98828f1b2
   artist2: "song2", 
   artist3: "song3"
 };
@@ -9,7 +13,13 @@ function updatePlaylist (obj, name, song){
   return obj;
 }
 
+<<<<<<< HEAD
  function removeFromPlaylist(obj, name){
    delete obj[name];
    return obj;
+=======
+ function removeFromPlaylist(playlist, artistName){
+   delete playlist.artistName;
+   return playlist;
+>>>>>>> ad16de4562245599b58210621b4279a98828f1b2
  }
